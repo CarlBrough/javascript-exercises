e0365b0aebf0e747e5ed22706bb3406c1b684167
Carl Brough <carl.brough@btinternet.com>
2020-02-20 19:41:25 +0000

    Changes made to meet spec

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, repeatValue) {
+        if (repeatValue < 0) return "ERROR";
+        let newString = "";
+        for (let i = 0; i < repeatValue; i++){
+          newString = newString + word;
+        }
+        return newString;
+      }
 
-}
 
 module.exports = repeatString
