f89c61ec414896336ba759068fe37bde81f2dca4
Carl Brough <carl.brough@btinternet.com>
2020-02-20 20:19:42 +0000

    my solution to reversestript

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let charLength = string.length;
+    let reversedString = "";
+        for (i = charLength; i > -1; i--) {
+            reversedString = reversedString + string.charAt(i);
+        }
+        return reversedString;
 }
 
 module.exports = reverseString
