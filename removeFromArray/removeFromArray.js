f9a6bc52e35e8ddde9647f8dc17fd07474e9df69
Carl Brough <carl.brough@btinternet.com>
2020-02-25 14:45:19 +0000

    my solution passed all specs

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, b, ...theArgs) {
+    let newArray = a;
+    for ( i = 0; i < (newArray.length); i++){
+        for (t = 1; t < arguments.length; t++)
+          if ( newArray[i] == arguments[t]){
+            newArray.splice(i,1);
+            i--;
+            
+          }
+        }
+        return newArray;
 }
 
 module.exports = removeFromArray
