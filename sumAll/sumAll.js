44740cc4268f90933d42316106c5d633d9cff957
Carl Brough <carl.brough@btinternet.com>
2020-02-28 14:30:56 +0000

    my solution to sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let total = 0;
+    let problem = "ERROR";
+    if (firstNumber < 0 || secondNumber < 0 || 
+    typeof firstNumber === "string" || typeof secondNumber === "string" || 
+    Array.isArray(firstNumber) || Array.isArray(secondNumber) ){
+      
+      return(problem);
+    }
+    if (firstNumber < secondNumber){
+      for (i = firstNumber; i <= secondNumber; i++ ) {
+        total = total + i;
+      }
+    }
+    else {
+      for (i = secondNumber; i <= firstNumber; i++ ){
+        total = total + i;
+      }
 
+      }
+      return(total);
 }
 
 module.exports = sumAll
