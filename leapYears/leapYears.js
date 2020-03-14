4a24293b6e88cc421b92fa2f473473afd74a1398
Carl Brough <carl.brough@btinternet.com>
2020-03-14 16:51:12 +0000

    my solution to leapYear problem

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
+    let answer = false;
+    if ( testYear % 4 === 0 &&( testYear % 100 !== 0 || testYear % 400 === 0 )){
+     answer = true;
 
+}
+return answer;
 }
 
 module.exports = leapYears
