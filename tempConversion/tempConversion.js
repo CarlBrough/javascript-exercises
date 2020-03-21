1a863ff7bbd7d7add8f258cbc25e4b56f9093d2b
Carl Brough <carl.brough@btinternet.com>
2020-03-21 23:22:27 +0000

    My solution to the two tempConversions functions

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenTemp) {
+  return ( Math.round(((fahrenTemp - 32) * 5 / 9) * 10) / 10);
 }
 
-const ctof = function() {
-
+const ctof = function(centiTemp) {
+  return ( Math.round ((centiTemp * 9 / 5 + 32) * 10) / 10);
 }
 
 module.exports = {
